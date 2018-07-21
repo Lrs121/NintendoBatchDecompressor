25364b17e373e8852fb9e1671fcb4191d93d0125
Rick Teuthof <rmt1998@hotmail.nl>
2018-07-21 18:19:17 +0200

    initial commit

---
+++ BatchDecompressor/BatchDecompressor.cs
@@ -0,0 +1,46 @@
+// For Directory.GetFiles and Directory.GetDirectories
+// For File.Exists, Directory.Exists
+
+using System;
+using System.IO;
+
+namespace BatchDecompressor
+{
+	public class BatchDecompressor
+	{
+		public static void Main(string[] args)
+		{
+//			foreach (var path in args)
+//				if (File.Exists(path)) {
+//					ProcessFile(path);
+//					Console.WriteLine("Processing file " + path);
+//				}
+//				else if (Directory.Exists(path))
+//					ProcessDirectory(path);
+//				else
+//					Console.WriteLine("{0} is not a valid file or directory.", path);
+			var path = @"D:\Downloads\Tex\stex\DevBu";
+			ProcessDirectory(path);
+			Console.WriteLine("Done.");
+			Console.ReadLine(); //Pause
+		}
+
+		// Process all files in the directory passed in and process the files they contain.
+		public static void ProcessDirectory(string targetDirectory)
+		{
+			// Process the list of files found in the directory.
+			var fileEntries = Directory.GetFiles(targetDirectory);
+			foreach (var fileName in fileEntries)
+				ProcessFile(fileName);
+		}
+
+		// Insert logic for processing found files here.
+		public static void ProcessFile(string path)
+		{
+			Stream fs = File.OpenRead(path);
+			var newPath = path + ".stex";
+			var decompressed = Nintendo.Decompress(fs);
+			File.WriteAllBytes(newPath, decompressed);
+		}
+	}
+}
---
+++ BatchDecompressor/Shared.cs
@@ -0,0 +1,8 @@
+﻿namespace BatchDecompressor
+{
+	public enum ByteOrder : ushort
+	{
+		LittleEndian = 0xFEFF,
+		BigEndian = 0xFFFE
+	}
+}
